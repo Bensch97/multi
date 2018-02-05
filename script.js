797fcc34ea969e9dd57cf5aa309c701bd8b01e15
Ben Schacht <Ben@2018BenSchacht.lan>
2018-02-05 15:14:02 -0500

    fixed math

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ for (x = 0; x < 11; x++) {
     document.getElementById("tableside").appendChild(vert);
     for (y=0; y < 11; y++) {
         var maths = document.createElement("p");
-        var mtext = document.createTextNode(x*x);
+        var mtext = document.createTextNode(x*y);
         maths.appendChild(mtext);
         document.getElementById("middle").appendChild(maths);
     }
