1931c29c3cf32c10a8be44297c7013d5833cee93
Ben Schacht <Ben@2018BenSchacht.lan>
2018-02-05 15:10:44 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+for (x = 0; x < 11; x++) {
+    var hori = document.createElement("p");
+    var vert = document.createElement("p");
+    var text = document.createTextNode(x);
+    var htext = document.createTextNode(x);
+    hori.appendChild(htext);
+    vert.appendChild(text);
+    document.getElementById("tabletop").appendChild(hori);
+    document.getElementById("tableside").appendChild(vert);
+    for (y=0; y < 11; y++) {
+        var maths = document.createElement("p");
+        var mtext = document.createTextNode(x*x);
+        maths.appendChild(mtext);
+        document.getElementById("middle").appendChild(maths);
+    }
+};
+// for (y = 0; y < 11; y++) {
+//     var vert = document.createElement("p");
+//     var text = document.createTextNode(y);
+//     vert.appendChild(text);
+//     document.getElementById("tableside").appendChild(vert);
+//     function thamaths ();
+//         y *
+// };
